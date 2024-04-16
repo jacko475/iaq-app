d1db8bd32d20265ddf69fef6407a5281b06f75f1
Jack <jacko475@gmail.com>
2024-04-16 10:58:55 +0800

    couple changes to query

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-
 const port = 5015;
 
-const dataRoute = require('./routes/realtimedata');
-const dataRoute2 = require('./routes/message2data');
+const dataRoute = require("./routes/realtimedata");
+const dataRoute2 = require("./routes/message2data");
 
-app.use('/realtimedata', dataRoute);
-app.use('/message2data', dataRoute2);
-app.use(express.static('public'));
+app.use("/realtimedata", dataRoute);
+app.use("/message2data", dataRoute2);
+app.use(express.static("public"));
 
 app.listen(port, () => {
-	console.log`listening on port ${port}`;
+	console.log(`listening on port ${port}`);
 });
